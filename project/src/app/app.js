3f33b15d4fce4d6baa83da141ba123879b468651
tse018 <60438717+tse018@users.noreply.github.com>
2022-03-09 13:54:50 +0100

    adding smooth scroll behavior

--- project/src/app/app.js
+++ project/src/app/app.js
@@ -1,10 +1,10 @@
-import { createApp } from 'vue';
-import { createStore } from 'vuex';
-import { createRouter, createWebHistory } from 'vue-router';
+import { createApp } from "vue";
+import { createStore } from "vuex";
+import { createRouter, createWebHistory } from "vue-router";
 
-import App from './App.vue';
-import routes from '../routes.js';
-import store from '../store.js';
+import App from "./App.vue";
+import routes from "../routes.js";
+import store from "../store.js";
 
 const app_store = createStore(store);
 
@@ -12,11 +12,15 @@ const router = createRouter({
    routes: routes,
    history: createWebHistory(),
    scrollBehavior() {
-      window.scrollTo(0,0);
+      window.scrollTo({
+         top: 0,
+         left: 0,
+         behavior: 'smooth'
+      });
    },
 });
 
 createApp(App)
    .use(app_store)
    .use(router)
-   .mount('#app'); 
+   .mount("#app");
