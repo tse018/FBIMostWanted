179a6af84a107700d90f7832e967e4df5b34921d
tse018 <60438717+tse018@users.noreply.github.com>
2022-06-01 19:04:44 +0200

    testing aria-label routerlink screen reader

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
    <header class="header-container">
-      <RouterLink class="header-container__link" :to="{ name: 'home' }">
+      <RouterLink class="header-container__link" :to="{ name: 'home' }" aria-label="go to home page">
          FBI Most Wanted
       </RouterLink>
 
